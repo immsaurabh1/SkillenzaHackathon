dc83802f4a1f67af7b6c3fcac734fd23f0654fb0
Saurabh Mishra <saurabh1512@live.com>
2019-08-31 23:56:23 +0530

    web app init

--- Predictionapp/src/App.js
+++ Predictionapp/src/App.js
@@ -1,56 +1,26 @@
-import React, { Component } from "react";
-import "./App.css";
-import { Route, Switch, withRouter } from "react-router-dom";
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import JssProvider from "react-jss/lib/JssProvider";
-import { create } from "jss";
-import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Axios from "axios";
-import styled from "styled-components";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import theme from "./theme";
-import PredictEmployeeAnalysis from "./components/PredictEmployeeAnalysis";
-import HackAppBar from "./components/SLAppBar";
-const generateClassName = createGenerateClassName();
-const jss = create({
-  ...jssPreset(),
-  insertionPoint: document.getElementById("jss-insertion-point")
-});
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {}
-  setUserAuthenticationDetails = () => {};
-  render() {
-    return (
-      <div>
-        <div className="App">
-          {/* <GlobalStyle /> */}
-          <React.Fragment>
-            <CssBaseline>
-              <JssProvider jss={jss} generateClassName={generateClassName}>
-                <MuiThemeProvider theme={theme}>
-                  {/* <Switch>
-                    <Route
-                      exact
-                      path={["*"]}
-                      render={renderProps => <PredictEmployeeAnalysis />}
-                    />
-                  </Switch> */}
-                  <Route path={["*"]} render={renderProps => <HackAppBar />} />
-                </MuiThemeProvider>
-              </JssProvider>
-            </CssBaseline>
-          </React.Fragment>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default withRouter(App);
+export default App;
